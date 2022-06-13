5daf9c93ad1967faf7b5c478f63a5024a0143af0
MFixed <100799910+MFixed@users.noreply.github.com>
2022-06-13 00:22:58 -0300

    comit

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Welcome to my Site" });
+  res.render('index', { title: "Titulo de la app" });
 });
 
 module.exports = router;
